c908d4c8a3bdcb23f6477c64bae3ca1b356152d6
jaison080 <jaisondennis080@gmail.com>
2022-11-10 21:49:21 +0530

    Minor BUg Fixes

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -12,11 +12,15 @@ import EditModal from "../components/EditModal/EditModal";
 import db from "../utils/firebase";
 function Dashboard() {
   const [orders, setOrders] = useState();
+  const [order,setOrder]=useState();
   const [loading, setLoading] = useState(true);
   const [open, setOpen] = useState(false);
   const [open1, setOpen1] = useState(false);
   const handleOpen = () => setOpen(true);
-  const handleOpen1 = () => setOpen1(true);
+  const handleOpen1 = (order) => {
+    setOpen1(true);
+    setOrder(order);
+  };
   async function getOrders() {
     let temp = [];
     const querySnapshot = await getDocs(collection(db, "orders"));
@@ -40,7 +44,6 @@ function Dashboard() {
       },
       { merge: true }
     );
-    window.location.reload(0);
   }
   async function incompleteOrder(id) {
     await setDoc(
@@ -50,11 +53,14 @@ function Dashboard() {
       },
       { merge: true }
     );
-    window.location.reload(0);
+  }
+  function editOrder(order)
+  {
+    <EditModal open={open1} setOpen={setOpen1} order={order} />
   }
   useEffect(() => {
     getOrders();
-  }, []);
+  }, [orders]);
   if (loading) {
     return <h1>Loading...</h1>;
   }
@@ -85,11 +91,11 @@ function Dashboard() {
             )}
 
             <button onClick={() => deleteOrder(order.id)}>Delete Order</button>
-            <button onClick={handleOpen1}>Edit Order</button>
-            <EditModal open={open1} setOpen={setOpen1} order={order} />
+            <button onClick={()=>{handleOpen1(order)}}>Edit Order</button>
           </div>
         );
       })}
+      <EditModal open={open1} setOpen={setOpen1} order={order} />
     </div>
   );
 }
